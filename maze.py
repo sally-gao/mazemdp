dd543baa666c2bc2be989c0e71e1e6f114f3c080
sally-gao <sg2zv@virginia.edu>
2018-02-21 23:33:27 -0500

    created Maze class with __repr__ method

---
+++ maze.py
@@ -0,0 +1,46 @@
+from random import shuffle, randrange
+
+class Maze:
+    def __init__(self, w=10, h=10):
+        """
+        Creates a maze of specified size.
+        Output is a list of lists, to be used as a matrix with coordinates.
+        '#' = wall
+        ' ' = empty space
+        
+        Adapted from http://rosettacode.org/wiki/Maze_generation#Python
+        """
+            
+        self.vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
+        self.ver = [["# "] * w + ['#'] for _ in range(h)] + [[]]
+        self.hor = [["##"] * w + ['#'] for _ in range(h + 1)]
+
+        self.walk(randrange(w), randrange(h))
+        
+        cells = ""
+        
+        for (a, b) in zip(self.hor, self.ver):
+            cells += ''.join(a + ['\n'] + b + ['\n'])
+            
+        cells = cells.split('\n')
+        cells = [list(row) for row in cells]
+        del cells[len(cells)-1]
+        del cells[len(cells)-1]
+        
+        self.matrix = [row for row in cells]
+        
+    def walk(self, x, y):
+        self.vis[y][x] = 1
+ 
+        d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
+        shuffle(d)
+        for (xx, yy) in d:
+            if self.vis[yy][xx]: continue
+            if xx == x: self.hor[max(y, yy)][x] = "# "
+            if yy == y: self.ver[y][max(x, xx)] = "  "
+            self.walk(xx, yy)
+ 
+    def __repr__(self):
+        strlst = [''.join(row) for row in self.matrix]
+        return '\n'.join(strlst)
+
