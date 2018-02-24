98cd771ca2beecf58283ffdd048c970edf48f227
Duncan Rule <wrule1@babson.edu>
2018-02-24 18:29:28 -0500

    Create MDP "state" object

---
+++ mdpstate.py
@@ -0,0 +1,14 @@
+
+class MDPState:
+    """State class for a given space in gridworld, with directional attributes pointing to other squares."""
+
+    def __init__(self, up, down, left, right, reward=-1, value=0):
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
+        self.reward = reward
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
